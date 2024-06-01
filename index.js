13185202145060020ee55d2ea1b2732561e446ca
ADudys <ania.dudys99@gmail.com>
2024-06-01 12:22:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const employee = {
-  personalInfo: {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    address: {
-      street: "123 Main St",
-      city: "Anytown",
-      state: "NY",
-      postalCode: "12345",
-    },
-  },
-  employmentDetails: {
-    position: "Software Engineer",
-    department: "Engineering",
-    startDate: "2022-01-01",
-    endDate: null,
-    manager: {
-      firstName: "Alice",
-      lastName: "Smith",
-      email: "alice@example.com",
-    },
-  },
-};
-console.log(employee)
+// const employee = {
+//   personalInfo: {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     address: {
+//       street: "123 Main St",
+//       city: "Anytown",
+//       state: "NY",
+//       postalCode: "12345",
+//     },
+//   },
+//   employmentDetails: {
+//     position: "Software Engineer",
+//     department: "Engineering",
+//     startDate: "2022-01-01",
+//     endDate: null,
+//     manager: {
+//       firstName: "Alice",
+//       lastName: "Smith",
+//       email: "alice@example.com",
+//     },
+//   },
+// };
+// console.log(employee)
 
-function getEmploeeInfo(key) {
-  const personalKeys = Object.keys(employee.personalInfo);
-  const employmentKeys = Object.keys(employee.employmentDetails);
+// function getEmploeeInfo(key) {
+//   const personalKeys = Object.keys(employee.personalInfo);
+//   const employmentKeys = Object.keys(employee.employmentDetails);
 
-  const keysArray = personalKeys.concat(employmentKeys);
+//   const keysArray = personalKeys.concat(employmentKeys);
 
-  if (keysArray.includes(key)) {
-    if (personalKeys.includes(key)) {
-      console.log(employee.personalInfo[key]);
-    }
+//   if (keysArray.includes(key)) {
+//     if (personalKeys.includes(key)) {
+//       console.log(employee.personalInfo[key]);
+//     }
 
-    if (employmentKeys.includes(key)) {
-      console.log(employee.employmentDetails[key]);
-    }
-  } else {
-    return console.log("Klucz nie istnieje");
-  }
-}
-getEmploeeInfo("firstName");
-getEmploeeInfo("lastName");
-getEmploeeInfo("position");
-getEmploeeInfo("street");
-getEmploeeInfo("address");
+//     if (employmentKeys.includes(key)) {
+//       console.log(employee.employmentDetails[key]);
+//     }
+//   } else {
+//     return console.log("Klucz nie istnieje");
+//   }
+// }
+// getEmploeeInfo("firstName");
+// getEmploeeInfo("lastName");
+// getEmploeeInfo("position");
+// getEmploeeInfo("street");
+// getEmploeeInfo("address");
